3ae782ccdc530d6391e477f60119c6276b4e7f20
giathanhphat <gtphat94@gmail.com>
2017-06-24 21:28:56 +0700

    test file php, ket qua van hien file text

---
+++ page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo 'test'; ?></title>
+</head>
+<body>
+	<?php 
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	echo 'chao ca nha';
+	?
+</body>
+</html>
